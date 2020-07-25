bbc00cfda62a1b6301ed9c90ba599f202f86283a
Viraj Shah <shahvirajd@gmail.com>
2020-07-24 20:03:00 -0400

    Cleaned up the project from clutter

--- src/Server.ts
+++ src/Server.ts
@@ -1,41 +1,38 @@
-import cookieParser from 'cookie-parser';
-import morgan from 'morgan';
-import path from 'path';
-import helmet from 'helmet';
+import cookieParser from "cookie-parser";
+import morgan from "morgan";
+import path from "path";
+import helmet from "helmet";
 
-import express, { Request, Response, NextFunction } from 'express';
-import { BAD_REQUEST } from 'http-status-codes';
-import 'express-async-errors';
+import express, { Request, Response, NextFunction } from "express";
+import { BAD_REQUEST } from "http-status-codes";
+import "express-async-errors";
 
-import BaseRouter from './routes';
-import logger from '@shared/Logger';
-
+import BaseRouter from "./routes";
+import logger from "./shared/Logger";
 
 // Init express
 const app = express();
-
-
 
 /************************************************************************************
  *                              Set basic express settings
  ***********************************************************************************/
 
 app.use(express.json());
-app.use(express.urlencoded({extended: true}));
+app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 
 // Show routes called in console during development
-if (process.env.NODE_ENV === 'development') {
-    app.use(morgan('dev'));
+if (process.env.NODE_ENV === "development") {
+    app.use(morgan("dev"));
 }
 
 // Security
-if (process.env.NODE_ENV === 'production') {
+if (process.env.NODE_ENV === "production") {
     app.use(helmet());
 }
 
 // Add APIs
-app.use('/api', BaseRouter);
+app.use("/", BaseRouter);
 
 // Print API errors
 app.use((err: Error, req: Request, res: Response, next: NextFunction) => {
@@ -45,19 +42,13 @@ app.use((err: Error, req: Request, res: Response, next: NextFunction) => {
     });
 });
 
-
-
 /************************************************************************************
  *                              Serve front-end content
  ***********************************************************************************/
 
-const viewsDir = path.join(__dirname, 'views');
-app.set('views', viewsDir);
-const staticDir = path.join(__dirname, 'public');
-app.use(express.static(staticDir));
-app.get('*', (req: Request, res: Response) => {
-    res.sendFile('index.html', {root: viewsDir});
-});
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "pug");
+app.use(express.static(path.join(__dirname, "public")));
 
 // Export express instance
 export default app;
--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,11 +1,15 @@
-import { Router } from 'express';
-import UserRouter from './Users';
+import { Router } from "express";
 
 // Init router and path
 const router = Router();
 
+// Base Routes
+router.get("/", (req, res) => {
+    res.render("landing");
+});
+
 // Add sub-routes
-router.use('/users', UserRouter);
+// router.use('/users', UserRouter);
 
 // Export the base-router
 export default router;
