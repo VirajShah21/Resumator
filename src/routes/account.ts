a199b1a61ab229276a1c0b02b56103f99a0f515b
Viraj Shah <shahvirajd@gmail.com>
2020-08-03 00:26:17 -0400

    Added separate router for /account

---
+++ src/routes/account.ts
@@ -0,0 +1,46 @@
+import { Router } from "express";
+import BodyParser from "body-parser";
+import Session from "@entities/Session";
+import Account from "@entities/Account";
+
+const router = Router();
+
+const jsonParser = BodyParser.json();
+
+router.get("/", (req, res) => {
+    res.render("account.pug", {
+        nav: "Home",
+    });
+});
+
+router.post("/signup", jsonParser, (req, res) => {
+    if (req.body.password == req.body.passwordconf) {
+        hashPassword(req.body.password, (hash) => {
+            let account = new Account(req.body.fname, req.body.lname, req.body.email, hash);
+            account.insertDatabaseItem((success) => {
+                if (success) {
+                    let session: Session = new Session(account);
+                    session.insertDatabaseItem(() => {
+                        res.cookie("session", session.key);
+                        res.redirect("/app/dashboard");
+                    });
+                } else {
+                    res.render("/app/signup-error", {
+                        nav: "Home",
+                    });
+                }
+            });
+        });
+    }
+});
+
+router.post("/update", (req, res) => {
+    Session.loadFromDatabase(req.cookies.session, (session) => {
+        Account.loadFromDatabase(session.user, (account) => {
+            // account.address =
+            // TODO: Add Validators
+        });
+    });
+});
+
+export default router;
--- src/routes/app-router.ts
+++ src/routes/app-router.ts
@@ -8,47 +8,12 @@ import Education from "@entities/Education";
 import path from "path";
 import app from "@server";
 import { ObjectId } from "mongodb";
+import AccountRouter from "./account";
 
 export const PREFIX = "/app";
 const router = Router();
 
 const jsonParser = BodyParser.json();
-
-router.get("/account", (req, res) => {
-    res.render("account.pug", {
-        nav: "Home",
-    });
-});
-
-router.post("/account/signup", jsonParser, (req, res) => {
-    if (req.body.password == req.body.passwordconf) {
-        hashPassword(req.body.password, (hash) => {
-            let account = new Account(req.body.fname, req.body.lname, req.body.email, hash);
-            account.insertDatabaseItem((success) => {
-                if (success) {
-                    let session: Session = new Session(account);
-                    session.insertDatabaseItem(() => {
-                        res.cookie("session", session.key);
-                        res.redirect("/app/dashboard");
-                    });
-                } else {
-                    res.render("/app/signup-error", {
-                        nav: "Home",
-                    });
-                }
-            });
-        });
-    }
-});
-
-router.post("account/update", (req, res) => {
-    Session.loadFromDatabase(req.cookies.session, (session) => {
-        Account.loadFromDatabase(session.user, (account) => {
-            // account.address =
-            // TODO: Add Validators
-        });
-    });
-});
 
 router.get("/dashboard", (req, res) => {
     Session.loadFromDatabase(req.cookies.session, (session) => {
