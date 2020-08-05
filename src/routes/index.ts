bb196b46ff3d463ba94ac7e29e151313e7909f63
Viraj Shah <shahvirajd@gmail.com>
2020-08-05 17:53:05 -0400

    Added middleware for logging requests

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,11 +1,35 @@
 import { Router } from "express";
 import AppRouter from "./app-router";
 import ApiRouter from "./api";
+import Logger from "@shared/Logger";
 
 export const PREFIX = "/";
 
 // Init router and path
 const router = Router();
+
+// Before any routes are handled
+router.use((req, res, next) => {
+    // Check if either req.query or req.body is empty
+    let isEmpty = true;
+    for (const query in req.query) {
+        if (req.query.hasOwnProperty(query)) {
+            isEmpty = false;
+            break;
+        }
+    }
+    if (isEmpty) {
+        for (const param in req.body) {
+            if (req.body.hasOwnProperty(param)) {
+                isEmpty = false;
+                break;
+            }
+        }
+    }
+
+    if (!isEmpty) Logger.info(`REQEST (${req.url}): ${JSON.stringify(req.body || req.query)}`);
+    next();
+});
 
 // Base Routes
 router.get("/", (req, res) => {
