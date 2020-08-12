ca5d7c9c0b0b582c62b475aa418405118fb84992
Viraj Shah <shahvirajd@gmail.com>
2020-08-12 16:42:59 -0400

    Cleaned up the logger

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -9,29 +9,6 @@ export const PREFIX = "/";
 // Init router and path
 const router = Router();
 
-// Before any routes are handled
-router.use((req, res, next) => {
-    // Check if either req.query or req.body is empty
-    let isEmpty = true;
-    for (const query in req.query) {
-        if (req.query.hasOwnProperty(query)) {
-            isEmpty = false;
-            break;
-        }
-    }
-    if (isEmpty) {
-        for (const param in req.body) {
-            if (req.body.hasOwnProperty(param)) {
-                isEmpty = false;
-                break;
-            }
-        }
-    }
-
-    if (!isEmpty) Logger.info(`REQEST (${req.url}): ${JSON.stringify(req.body || req.query)}`);
-    next();
-});
-
 // Base Routes
 router.get("/", (req, res) => {
     res.render(views.landingPage, {
