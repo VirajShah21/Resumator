af6f7a17155a834d798eb0b4d15858fb2d8d4441
Viraj Shah <shahvirajd@gmail.com>
2020-08-12 15:39:15 -0400

    Added a dashboard puggable

---
+++ src/entities/puggables/DashboardPuggableAccess.ts
@@ -0,0 +1,9 @@
+export default class DashboardPuggableAccess {
+    public email: string;
+
+    constructor(email: string) {
+        this.email = email;
+    }
+
+    public gather(): DashboardPuggableAccess {}
+}
