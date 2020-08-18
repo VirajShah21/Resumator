80f30da1d88ed2da6398c30dd6c0a2ff8c18d41c
Viraj Shah <shahvirajd@gmail.com>
2020-08-18 15:54:16 -0400

    Minor Feature: Added more tips for undergraduate interns

--- src/shared/util/GoalParser.ts
+++ src/shared/util/GoalParser.ts
@@ -35,27 +35,36 @@ function handleUGIInternship(resumeInfo: ResumeInfo): IGoalResults {
         resumeInfo.educationHistory.filter((edu) => {
             return edu.level === "Undergraduate";
         }).length === 0
-    ) {
+    )
         results.requirements.push({
             for: "education",
             message:
                 "When applying for an undergraduate internship, it is required that you have at least one undergraduate education listed",
         });
-    }
 
-    const goodSkills = resumeInfo.skillset.filter((skill) => {
-        return skill.proficiency >= 0.67;
-    });
-
-    if (goodSkills.length < 5) {
+    if (
+        resumeInfo.skillset.filter((skill) => {
+            return skill.proficiency >= 0.67;
+        }).length < 5
+    )
         results.requirements.push({
             for: "skills",
             message:
                 "You should have at least 5 skills... an internship is a job, where you use your existing skills to learn your field more in depth!",
         });
-    }
 
-    if (resumeInfo.workExperience.length === 0) {
+    if (
+        resumeInfo.educationHistory.filter((edu) => {
+            return edu.level === "High School";
+        }).length === 0 &&
+        resumeInfo.educationHistory.length > 0
+    )
+        results.tips.push({
+            for: "education",
+            message: "Adding a high school to your resume would strengthen your education history",
+        });
+
+    if (resumeInfo.workExperience.length === 0)
         results.tips.push({
             for: "work",
             message:
@@ -63,7 +72,6 @@ function handleUGIInternship(resumeInfo: ResumeInfo): IGoalResults {
             helpLink: "#",
             helpMessage: "What work experience should I list?",
         });
-    }
 
     return results;
 }
