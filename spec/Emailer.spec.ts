248075a4fabd0f30318cb99c76a7f7115cad520e
Viraj Shah <shahvirajd@gmail.com>
2020-12-28 22:17:38 -0500

    Initialized project for unit testing

---
+++ spec/Emailer.spec.ts
@@ -0,0 +1,8 @@
+import { VerifyEmailer } from '@shared/util/Emailer';
+import { ObjectId } from 'mongodb';
+
+describe('Test for @shared/util/Emailer.ts', () => {
+    it('VerifyEmailer can be constructed', () => {
+        new VerifyEmailer(new ObjectId());
+    });
+});
