40269855b0e971db6f82ab59c79dca620c151fdf
Viraj Shah <shahvirajd@gmail.com>
2021-01-02 17:26:49 -0500

    Lowered jest goal to 20% coverage

--- jest.config.js
+++ jest.config.js
@@ -6,10 +6,10 @@ module.exports = {
     },
     coverageThreshold: {
         global: {
-            branches: 80,
-            functions: 80,
-            lines: 80,
-            statements: 80,
+            branches: 20,
+            functions: 20,
+            lines: 20,
+            statements: 20,
         },
     },
     collectCoverageFrom: [
